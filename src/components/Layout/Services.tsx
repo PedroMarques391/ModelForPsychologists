b87781c44d0a4d739f1e32d6227394e76e81fb2d
Pedro <pedromarques391@gmail.com>
2024-05-06 13:49:29 -0300

    feat: Adicioando contador dinâmico para horas de sessões e total de clientes atendidos no componente "Services.tsx"

--- src/components/Layout/Services.tsx
+++ src/components/Layout/Services.tsx
@@ -1,9 +1,30 @@
 import Card from "../Card"
 import { IconHeart, IconWindow, IconUsers } from "../Auxiliary/Icons"
-
+import { useEffect, useState } from "react";
 
 
 const Services = (): React.JSX.Element => {
+    const [clients, setClients] = useState(0);
+    const [hour, setHour] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (clients < 100) {
+                setClients(clients + 1);
+            }
+        }, 10);
+        return () => clearInterval(interval);
+    }, [clients]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (hour < 1800) {
+                setHour(hour + 1);
+            }
+        }, 1);
+        return () => clearInterval(interval);
+    }, [hour]);
+
     return (
         <section className='flex w-full sm:w-3/5 md:w-[90%]'>
             <div
@@ -11,7 +32,7 @@ const Services = (): React.JSX.Element => {
                 className='mx-auto flex flex-col sm:flex-row gap-0 sm:gap-20 md:flex-wrap md:gap-10 md:justify-center'>
                 <Card
                     aos='fade-up'
-                    text={"Transformamos vidas: mais de 100 clientes atendidos com sucesso em 2023, totalizando mais de 1800 sessões de terapia online."}
+                    text={`Transformamos vidas: mais de ${clients} clientes atendidos com sucesso em 2023, totalizando mais de ${hour} horas de sessões de terapia online.`}
                     icon={IconHeart}
                 />
                 <Card
